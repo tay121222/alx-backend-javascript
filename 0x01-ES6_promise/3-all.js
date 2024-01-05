ba86cab250a97cb00796f1e93f2e258d06d0a508
Enoch Taylor <tay121222@gmail.com>
2024-01-05 23:16:44 +0000

    Handle multiple successful promises task 3 fixed return error on reject

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -5,7 +5,8 @@ export default function handleProfileSignup() {
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
-    .catch(() => {
+    .catch((error) => {
       console.error('Signup system offline');
+      throw error;
     });
 }
