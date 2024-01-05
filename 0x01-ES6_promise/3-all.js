5372a2b7267be9d99db7d859af89a89b258fb678
Enoch Taylor <tay121222@gmail.com>
2024-01-05 22:59:50 +0000

    Handle multiple successful promises task 3 fixed to return promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,8 +1,7 @@
-import { uploadPhoto, createUser } from '../utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise
-    .all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
       console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
     })
