08856fd50535423aa370c4cf4daa24724d7c7f17
Enoch Taylor <tay121222@gmail.com>
2024-01-05 22:18:19 +0000

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((data) => {
+    .then(() => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
     })
-    .catch((error) => {
+    .catch(() => {
       console.error('Got an error from the API');
-      return {};
+      return new Error();
     });
 }
