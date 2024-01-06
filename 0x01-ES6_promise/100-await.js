59827e6b3b481df63eae77b71ea5f4a485f27417
Enoch Taylor <tay121222@gmail.com>
2024-01-06 12:23:10 +0000

    async function named asyncUploadUser that will call these two functions and return an object try 1

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -2,12 +2,15 @@ import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
-    const [photo, user] = await Promise.allSettled([
+    const [photoResult, userResult] = await Promise.allSettled([
       uploadPhoto(),
       createUser(),
     ]);
 
-    return { photo: photo.value, user: user.value };
+    const photo = photoResult.status === 'fulfilled' ? photoResult.value : null;
+    const user = userResult.status === 'fulfilled' ? userResult.value : null;
+
+    return { photo, user };
   } catch (error) {
     return { photo: null, user: null };
   }
