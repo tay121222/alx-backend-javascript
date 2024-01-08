c0be0730771ffb8fd4251895daa39593cb1f42ca
Enoch Taylor <tay121222@gmail.com>
2024-01-08 23:03:53 +0000

    Trying to fix lint error

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,20 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (!Array.isArray(students)) {
+      throw new TypeError('Students must be an Array');
+    }
+
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
@@ -10,9 +22,6 @@ export default class HolbertonCourse {
   }
 
   set name(newName) {
-    if (typeof newName !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
     this._name = newName;
   }
 
@@ -22,19 +31,13 @@ export default class HolbertonCourse {
 
   set length(newLength) {
     this._length = newLength;
-    if (typeof newLength !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
   }
 
-   get students() {
+  get students() {
     return this._students;
   }
 
-  set students(newStudents) {
-    if (!Array.isArray(newStudents)) {
-      throw new TypeError('Students must be an Array');
-    }
-    this._students = newStudents;
+  set students(NewStudents) {
+    this._students = NewStudents;
   }
 }
