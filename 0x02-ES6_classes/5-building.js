ead668066c238233ba0880d3e9eaa623c278577b
Enoch Taylor <tay121222@gmail.com>
2024-01-09 15:29:59 +0000

    A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,11 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        this.evacuationWarningMessage();
-      }
+    if (
+      this.constructor !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage
+    ) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
 
