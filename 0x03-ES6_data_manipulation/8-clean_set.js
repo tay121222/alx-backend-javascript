aecfe1bc9e9b08a8ca6eb16eb5760e1ab051bd21
Enoch Taylor <tay121222@gmail.com>
2024-01-11 13:39:33 +0000

    Trying to fix to return expected string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (typeof startString !== 'string' || startString === '') {
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
     return '';
   }
 
   const filValues = Array.from(set)
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length));
+    .filter((value) => typeof value === 'string' && value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .filter((value, index, self) => value && value !== self[index - 1]);
 
-  return filValues.length > 0 ? filValues.join('-') : ' ';
+  return filValues.length > 0 ? filValues.join('-') : '';
 }
