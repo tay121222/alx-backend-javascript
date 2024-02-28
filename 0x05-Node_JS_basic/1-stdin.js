66b1171c3a935ef2d2f0e3c89cd7cc32ad78365e
Enoch Taylor <tay121222@gmail.com>
2024-02-28 04:42:10 +0000

    Fix task One

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const input = data.toString().trim();
   if (input !== '') {
     process.stdout.write(`Your name is: ${input}\n`);
-  } else {
-    process.stdout.write('Your name is: \n');
   }
 });
 
