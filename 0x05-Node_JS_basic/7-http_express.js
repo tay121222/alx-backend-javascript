cba2002b5620d818ad47ee057f14bf77f0b21e5d
Enoch Taylor <tay121222@gmail.com>
2024-02-29 09:40:28 +0000

    recreate the small HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const fs = require('fs').promises;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const dataPath = process.argv[2];
+    const data = await fs.readFile(dataPath, 'utf8');
+    const lines = data.trim().split('\n').filter((line) => line.trim() !== '');
+
+    const studentGroups = {};
+    const fieldNames = lines[0].split(',').map((name) => name.trim());
+    let response = 'This is the list of our students\n';
+    response += `Number of students: ${lines.length - 1}\n`;
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const fields = lines[i].split(',');
+      const field = fields.pop().trim();
+      const student = {};
+      fields.forEach((value, index) => {
+        student[fieldNames[index]] = value.trim();
+      });
+
+      if (!studentGroups[field]) {
+        studentGroups[field] = [];
+      }
+
+      studentGroups[field].push(student);
+    }
+
+    for (const [field, students] of Object.entries(studentGroups)) {
+      const studentNames = students.map((student) => student.firstname).join(', ');
+      response += `Number of students in ${field}: ${students.length}. List: ${studentNames}\n`;
+    }
+    response = response.trim();
+    res.end(response);
+  } catch (error) {
+    console.error('Error loading database:', error.message);
+    let errresponse = 'This is the list of our students\n';
+    errresponse += 'Cannot load the database';
+    res.end(errresponse);
+  }
+});
+
+const PORT = 1245;
+app.listen(PORT, () => {
+  console.log(`Server is running and listening on port ${PORT}`);
+});
+
+module.exports = app;
