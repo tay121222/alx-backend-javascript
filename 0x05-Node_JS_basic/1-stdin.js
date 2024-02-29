83338a79a9f1d4809e4de2bf04b02ff755f5f090
Enoch Taylor <tay121222@gmail.com>
2024-02-29 17:06:17 +0000

    A fix to Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  if (input !== '') {
-    process.stdout.write(`Your name is: ${input}\n`);
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    const input = chunk.toString().trim();
+    if (input !== '') {
+      process.stdout.write(`Your name is: ${input}\n`);
+    }
   }
 });
 
-process.on('exit', () => console.log('This important software is now closing'));
+process.stdin.on('end', () => console.log('This important software is now closing'));
