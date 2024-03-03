411054aaeebb0a5066db4582c86e1735b4875510
Enoch Taylor <tay121222@gmail.com>
2024-03-03 21:59:17 +0000

    Fix test for second number rounded - 1

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -17,4 +17,8 @@ describe('calculateNumber', function () {
   it('should return 6 when the first number is rounded to 2 and the second number is rounded to 4', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+
+  it('should return 4 when the second number is rounded to 3', function () {
+    assert.strictEqual(calculateNumber(1.0, 2.5), 4);
+  });
 });
