2e9c23afd5e40ac4fdbe11d6f77cada47cad6432
Enoch Taylor <tay121222@gmail.com>
2024-03-03 21:54:10 +0000

    Fix test for second number rounded

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,10 +2,19 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
-  it('should return the sum of rounded numbers', function () {
+  it('should return 4 when both numbers are rounded to 1 and 3 respectively', function () {
     assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('should return 5 when the first number is rounded to 1 and the second number is rounded to 4', function () {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('should return 5 when the first number is rounded to 1 and the second number is rounded to 4', function () {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('should return 6 when the first number is rounded to 2 and the second number is rounded to 4', function () {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
