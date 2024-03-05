9c7e9fdc5ce975ee2fba5d5f3ce7bb6918ce50c1
Enoch Taylor <tay121222@gmail.com>
2024-03-05 05:10:53 +0000

    Regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,34 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Basic Integration testing', () => {
+  it('Correct status code?', (done) => {
+    request.get('http://localhost:7865', (error, response) => {
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+  });
+
+  it('Correct result?', (done) => {
+    request.get('http://localhost:7865', (error, response, body) => {
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+    });
+  });
+
+
+  it('Correct status code when :id is a number?', (done) => {
+    request.get('http://localhost:7865/cart/12', (error, response) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('Correct status code when :id is NOT a number (=> 404)?', (done) => {
+    request.get('http://localhost:7865/cart/hello', (error, response) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+
+});
